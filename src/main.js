e1ce311d7b26b9d773ba39f15da820e660ad4828
BarryCN30 <xuwangcn1202@gmail.com>
2022-05-18 21:27:06 +0800

    3-项目的基本优化

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 const Koa =require('koa')
 
+const{APP_PORT} = require('./config/config.default')
+
+
 const app = new Koa()
 
 app.use((ctx, next) =>{
     ctx.body = 'hello world'
 })
 
-app.listen(3000,() => {
-    console.log('server is running on http://localhost:3000')
+app.listen(APP_PORT,() => {
+    console.log(`server is running on http://localhost:${APP_PORT}`)
 })
