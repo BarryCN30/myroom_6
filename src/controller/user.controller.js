9aec097b9cc9c717eb0aba7ee8d72f0cd7ca48e9
BarryCN30 <xuwangcn1202@gmail.com>
2022-05-19 16:42:27 +0800

    6-解析body

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,8 +1,19 @@
 const { use } = require("../router/user.route")
+const { createUser } = require("../service/user.service")
 
 class UserController{
     async register(ctx, next){
-        ctx.body = '用户注册成功'
+        //1.获取数据
+        //console.log(ctx.request.body)
+        const {username, password} = ctx.request.body
+        
+        //2.操作数据库
+        const res = await createUser(username, password)
+        console.log(res)
+        //3.返回结果
+
+
+        ctx.body = ctx.request.body
     }
 
     async login(ctx, next){
