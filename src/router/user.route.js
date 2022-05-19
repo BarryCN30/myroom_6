b49822b92ed14d70b834bda70fa1f1100a2fc113
BarryCN30 <xuwangcn1202@gmail.com>
2022-05-19 15:54:59 +0800

    将路由和控制器拆分

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,10 +1,18 @@
 const Router = require('koa-router')
 
+const {register, login} = require('../controller/user.controller')
+
 const router = new Router({prefix:'/users'})
 
+//注册接口
+router.post('/register', register)
+
 //Get /users/拼接
-router.get('/', (ctx,next)=>{
-    ctx.body = 'hello users'
-})
+// router.get('/', (ctx,next)=>{
+//     ctx.body = 'hello users'
+// })
+
+//登陆接口
+router.post('/login', login)
 
 module.exports = router
