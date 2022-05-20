9aa7852f8381c40cc978c08ecf51f8a8a606ce9a
BarryCN30 <xuwangcn1202@gmail.com>
2022-05-20 11:22:34 +0800

    7-错误处理

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,19 +1,48 @@
 const { use } = require("../router/user.route")
-const { createUser } = require("../service/user.service")
+const { createUser, getUserInfo} = require("../service/user.service")
 
 class UserController{
     async register(ctx, next){
         //1.获取数据
         //console.log(ctx.request.body)
         const {username, password} = ctx.request.body
-        
+        //合法性
+        if(!username || !password){
+            console.error('用户名或密码为空', ctx.request.body);
+            ctx.status = 400//请求格式
+            ctx.body ={
+                code: '10001',
+                message: '用户名或密码为空',
+                result: '',
+            }
+            return
+        }
+        //合理性
+        //如果用户存在就不重复添加
+        if(getUserInfo({username})){
+            ctx.status = 409
+            ctx.body ={
+                code : '10002',
+                message: '用户已经存在',
+                result: ''
+            }
+            return
+        }
+
+
         //2.操作数据库
         const res = await createUser(username, password)
-        console.log(res)
         //3.返回结果
 
 
-        ctx.body = ctx.request.body
+        ctx.body = {
+            code : 0,
+            message: '用户注册成功',
+            result: {
+                id: res.id,
+                username: res.username,
+            },
+        }
     }
 
     async login(ctx, next){
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,7 +1,29 @@
+const User = require('../model/user.model')
+
 class UserService{
     async createUser(username, password){
         //todo:写入数据库
-        return '写入数据库成功'
+        const res = await User.create({username, password })
+        // console.log(res)
+
+        return res.dataValues
+        // User.create({
+        //     username: username,
+        //     password: password
+        // })
+    }
+    async getUserInfo({id,username,password}){
+        const whereOpt = {}
+
+        id && Object.assign(whereOpt,{ id }),
+        username && Object.assign(whereOpt,{ username }),
+        password && Object.assign(whereOpt,{ password })
+
+        const res = await User.findOne({
+            attributes: ['id', 'username', 'password'],
+            where: whereOpt
+        })
+        return res ? res.dataValues : null
     }
 }
 
